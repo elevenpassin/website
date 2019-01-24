17ac20baf886cdfe85b43981125f535c886f859e
buoyantair <buoyantair@protonmail.com>
2019-01-24 11:14:24 +0530

    Update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,12 @@ import React from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import Title from '../components/Title'
-
+import './about.css'
+import resume from '../assets/resume.pdf'
 const SecondPage = () => (
   <Layout>
     <SEO
-      title="About buoyantair"
+      title="about"
       keywords={[
         `buoyantair`,
         `frontend`,
@@ -18,7 +19,13 @@ const SecondPage = () => (
         'developer',
       ]}
     />
-    <Title big={true}>about buoyantair</Title> <p>Hello!</p>
+    <Title big={true}>about buoyantair</Title>
+    <section>
+      Hey! I'm a javascript developer working on <i>free</i> and open source
+      software. I have experience working in teams, designing and developing
+      word-class projects using various modern web technologies. <br />
+      You can find my resume <a href={resume}>here.</a>
+    </section>
   </Layout>
 )
 

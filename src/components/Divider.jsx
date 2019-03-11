1592dfb7df3816ace6e800f049342f033807fa73
buoyantair <buoyantair@protonmail.com>
2019-03-12 02:05:37 +0530

    Simple frontpage
    - Wrote Section & Divider Components
    - Moved data logic from Layout to IndexPage
    - Add Normalize, CSS Reset & Fonts

---
+++ src/components/Divider.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default () => (
+  <svg
+    className="divider"
+    width="58"
+    height="14"
+    viewBox="0 0 58 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <line y1="6.75482" x2="58" y2="6.75482" stroke="black" />
+    <path
+      d="M29.5 0L36.3259 6.82588L29.5 13.6518L22.6741 6.82588L29.5 0Z"
+      fill="black"
+    />
+    <path
+      d="M38 6.82588L42.3482 11.1741L46.6965 6.82588L42.3482 2.47764L38 6.82588Z"
+      fill="black"
+    />
+    <path
+      d="M12 6.82588L16.3482 11.1741L20.6965 6.82588L16.3482 2.47764L12 6.82588Z"
+      fill="black"
+    />
+  </svg>
+)

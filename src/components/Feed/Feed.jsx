225cd9c05f181998eefe01ad154bbddf36d9e3bf
buoyantair <buoyantair@protonmail.com>
2019-03-12 17:52:04 +0530

    fix feed & use error boundary

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,84 +1,78 @@
-import React, { useState, useEffect } from "react"
+import React, { Component } from "react"
 
 import FeedEntry from "../FeedEntry"
+import ErrorBoundary from "../ErrorBoundary"
 import "./feed.css"
 
 const InvertedHeading = ({ children }) => (
   <h1 className="inverted-heading">{children}</h1>
 )
 
-export default () => {
-  const [blogPosts, setBlogPosts] = useState([])
-  const [contributions, setContributions] = useState([])
+export default class Feed extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      blogPosts: [],
+      contributions: [],
+    }
+  }
 
-  // const fetchData = async () => {
-  // try {
-  //   let posts = await fetch(
-  //     "https://public-api.wordpress.com/rest/v1.1/sites/buoyantair.wordpress.com/posts/"
-  //   )
-  //   posts = await posts.json()
-  //   setBlogPosts(posts)
-  //   let contribs = await fetch(
-  //     "https://api.github.com/users/buoyantair/events"
-  //   )
-  //   contribs = await contribs.json()
-  //   // contribs = contribs.filter(x => x.type === "PullRequestEvent")
-  //   setContributions(contribs)
-  // } catch (e) {
-  //   console.error(e)
-  // }
-  // }
-
-  useEffect(() => {
+  componentDidMount() {
     fetch(
-      "https://public-api.wordpress.com/rest/v1.1/sites/buoyantair.wordpress.com/posts/"
+      'https://public-api.wordpress.com/rest/v1.1/sites/buoyantair.wordpress.com/posts/'
     )
       .then(resp => resp.json())
       .then(data => {
-        setBlogPosts(data.posts)
+        this.setState({
+          blogPosts: data.posts,
+        })
       })
       .catch(console.error)
-    // fetch("https://api.github.com/users/buoyantair/events")
-    //   .then(resp => resp.json())
-    //   .then(events => {
-    //     console.log(events)
-    //     return events.filter(x => x.type === "PullRequestEvent")
-    //   })
-    //   .then(data => {
-    //     setContributions(data)
-    //   })
-    //   .catch(console.error)
-  })
+    fetch('https://api.github.com/users/buoyantair/events')
+      .then(resp => resp.json())
+      .then(events => events.filter(x => x.type === 'PullRequestEvent'))
+      .then(data => {
+        this.setState({
+          contributions: data,
+        })
+      })
+      .catch(console.error)
+  }
 
-  const renderList = [...blogPosts, ...contributions]
-  renderList
-    .sort((a, b) => {
-      const aTime = Date.parse(a.created_at) || Date.parse(a.date)
-      const bTime = Date.parse(b.created_at) || Date.parse(b.date)
+  render() {
+    const { blogPosts, contributions } = this.state
+    const renderList = [...blogPosts, ...contributions]
+    renderList
+      .sort((a, b) => {
+        const aTime = Date.parse(a.created_at) || Date.parse(a.date)
+        const bTime = Date.parse(b.created_at) || Date.parse(b.date)
 
-      if (aTime > bTime) {
-        return 1
-      } else if (aTime < bTime) {
-        return -1
-      } else {
-        return 0
-      }
-    })
-    .reverse()
-
-  return (
-    <div className="feed">
-      <InvertedHeading>
-        photons from the far side of the universe
-      </InvertedHeading>
-      <section className="entry-list">
-        {(renderList &&
-          renderList.length &&
-          renderList.map((entry, index) => (
-            <FeedEntry key={entry.ID || entry.id || index} entry={entry} />
-          ))) ||
-          null}
-      </section>
-    </div>
-  )
+        if (aTime > bTime) {
+          return 1
+        } else if (aTime < bTime) {
+          return -1
+        } else {
+          return 0
+        }
+      })
+      .reverse()
+    
+    return (
+      <ErrorBoundary>
+        <div className="feed">
+          <InvertedHeading>
+            photons from the far side of the universe
+          </InvertedHeading>
+          <section className="entry-list">
+            {(renderList &&
+              renderList.length &&
+              renderList.map((entry, index) => (
+                <FeedEntry key={entry.ID || entry.id || index} entry={entry} />
+              ))) ||
+              null}
+          </section>
+        </div>
+      </ErrorBoundary>
+    )
+  }
 }

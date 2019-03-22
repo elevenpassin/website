14de4d7a2eee898c9dcd3525986a3823a55dd623
buoyantair <buoyantair@protonmail.com>
2019-03-22 17:50:05 +0530

    fix: change <Link /> component into a basic anchor tag

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Section from "../components/Section"
@@ -19,7 +18,11 @@ const AboutPage = () => (
         Hey! I'm a javascript developer working on free and open source
         software. I have experience working in teams, designing and developing
         word-class projects using various modern web technologies. You can find
-        my resume <Link to={Resume}>here</Link>.
+        my resume{" "}
+        <a href={Resume} alt="Download Resume">
+          here
+        </a>
+        .
       </p>
       <Divider />
     </Section>
